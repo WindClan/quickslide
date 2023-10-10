385413518c65461da52b2d5f45a3077ff5d9d6e0
Ian <slimit75@pm.me>
2023-10-09 21:42:03 -0400

    chore: remove random AGPL reference

--- libs/qs_utils.lua
+++ libs/qs_utils.lua
@@ -1,18 +1,3 @@
---[[
-	QuickSlide Utils
-	Copyright (C) 2023  slimit75
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published
-	by the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-]]--
-
 function print_info(string)
     term.blit("INFO: ","33338f","ffffff")
     print(string)
